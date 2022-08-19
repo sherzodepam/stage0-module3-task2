b95f1d5f35f2f3a64c92016312d8e6b4f5452a33
Sherzod <sherzodd7707@gmail.com>
2022-08-19 09:07:33 +0500

    mainly i change variable names

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,13 +2,18 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable = 0;
-        int variable = 2000;
-        int value = 1000;
-        int variable = 4000;
+        int variable1 = 0;
+        int variable2 = 2000;
+        int value1 = 1000;
+        int variable3 = 4000;
         int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
-        int value = 90000;
+        int variable4 = 5000;
+        int variable5 = 7000;
+        int value3 = 90000;
+        int Int=100000;
+        int myInt= 1000000;
+        int myVar = 1234567;
+        int myInt2 = 1234567;
+
     }
 }
