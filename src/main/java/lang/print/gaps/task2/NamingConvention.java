13df7d1aa35948392142f61bac311f9845d8b10c
Sherzod <sherzodd7707@gmail.com>
2022-08-21 12:03:50 +0500

    I changed varriable with valid name>>>

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task2;
 public class NamingConvention {
     public static void main(String[] args) {
         int variable=10;
-        int variableOne = 1;
+        int variable_one = 1;
         int variableTwo = 2;
         int variableThree = 3;
         final int VARIABLE_FOUR = 4;
