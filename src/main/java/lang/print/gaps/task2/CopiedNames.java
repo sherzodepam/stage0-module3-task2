b923e407c585f354253139a0b3629d1d982ca941
Sherzod <sherzodd7707@gmail.com>
2022-08-21 12:01:48 +0500

    I changed varriable with valid name

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,18 +2,18 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable1 = 0;
-        int variable2 = 2000;
-        int value1 = 1000;
-        int variable3 = 4000;
-        int value2 = 3000;
-        int variable4 = 5000;
-        int variable5 = 7000;
-        int value3 = 90000;
+        int variableOne = 0;
+        int variableTwo = 2000;
+        int valueOne = 1000;
+        int variableThree = 4000;
+        int value_two = 3000;
+        int variableFour = 5000;
+        int variable_five = 7000;
+        int valueThree = 90000;
         int Int=100000;
         int myInt= 1000000;
         int myVar = 1234567;
-        int myInt2 = 1234567;
+        int myIntTwo = 1234567;
 
     }
 }
